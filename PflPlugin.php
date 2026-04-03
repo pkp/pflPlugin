75ce39d24413eeaa43c21a21c9432eae876f05ad
Alec Smecher <alec@smecher.bc.ca>
2026-04-02 18:09:03 -0700

    #66 Show correct days to publication statistic

--- PflPlugin.php
+++ PflPlugin.php
@@ -320,7 +320,7 @@ class PflPlugin extends GenericPlugin {
                     'pflAcceptedPercent' => __('plugins.generic.pfl.percentage', ['num' => $acceptanceRate]),
                     'pflNumAcceptedClass' => __('plugins.generic.pfl.percentage', ['num' => $statistics['pflNumAcceptedClass']]),
                     'pflDaysToPublication' => $publicationDate->diff($submissionDate)->format('%a'),
-                    'pflDaysToPublicationClass' =>  $statistics['pflNumAcceptedClass'],
+                    'pflDaysToPublicationClass' =>  $statistics['pflDaysToPublicationClass'],
                     'pflIndexList' => $pflIndexListTransformed,
                     'editorialTeamUrl' => $router->url($request, null, 'about', 'editorialTeam'),
                     'pflAcademicSociety' => $this->getSetting($journal->getId(), 'academicSociety') ?? 'NA',
