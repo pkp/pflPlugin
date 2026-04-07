f3e7428aac97c426d93bd2c466310d15064f45b7
Alec Smecher <alec@smecher.bc.ca>
2026-04-07 13:43:59 -0700

    #67 Exclude zero-or-less times to publication from average calculation

--- PflPlugin.php
+++ PflPlugin.php
@@ -126,6 +126,7 @@ class PflPlugin extends GenericPlugin {
                 JOIN publications p ON (s.current_publication_id = p.publication_id)
                 JOIN sections sec ON (p.section_id = sec.section_id)
                 WHERE s.context_id = ' . ((int) $journalId) . ' AND sec.meta_reviewed = 1 AND s.status = ' . ((int) PKPSubmission::STATUS_PUBLISHED) . '
+                AND p.date_published > s.date_submitted
                 ' . ($dateStart ? ' AND s.date_submitted >= ' . strtotime($dateStart) : '') . '
                 GROUP BY p.publication_id, s.submission_id
             ) a'
